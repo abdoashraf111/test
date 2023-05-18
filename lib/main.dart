0dfc53852ddb0fe89e6d283017b70c21de6e1c30
abdelrahman ashraf <63114136+abdoashraf111@users.noreply.github.com>
2023-05-18 16:57:55 +0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:testt/test.dart';
-import 'package:testt/test_cubit.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -13,11 +12,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => TestCubit(),
-      child: const MaterialApp(
-        home: Test(),
-      ),
-    );
+    return const MaterialApp(
+      home: Scaffold(),
+    ) ;
+
   }
 }
